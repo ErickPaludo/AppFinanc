790befd7379da1a0cef4aa5d6cbd90d5e89cdc92
Érick Augusto Paludo <erickpaludoyt@gmail.com>
2024-09-13 23:59:19 -0300

    Salvamento

--- MVVM/ModelViews/ComponentesVM/ValueRegisterVM.cs
+++ MVVM/ModelViews/ComponentesVM/ValueRegisterVM.cs
@@ -16,20 +16,47 @@ namespace AppFinanc.MVVM.ModelViews.ComponentesVM
         [ObservableProperty]
         private bool _isRedButtonEnabled;
 
-        private SqliteService sql;
+        [ObservableProperty]
+        private TableFinanc _dados;
+
+        [ObservableProperty]
+        private List<TableFinanc> _data;
+
+        private SqliteService sql = SqliteService.GetInstance();
+
+
 
         [RelayCommand]
         private void ValueRegisterViewModel()
         {
             IsRedButtonEnabled = !IsRedButtonEnabled;
-            sql.InitilizeAsync();
         }
 
         public ValueRegisterVM()
         {
-            sql = SqliteService.GetInstance();
+   
         }
 
+        private async Task DisplayCommand()
+        {
+            await sql.InitilizeAsync();
+            await Refresh();
+            if (Data.Count > 0)
+            {
+                var obj = Data[0];
+                Dados = new TableFinanc
+                {
+                 id = obj.id,
+                 gasto = obj.gasto,
+                 tipo = obj.tipo,
+                 valor = obj.valor,
+                };
+            }
+        }
+        private async Task Refresh()
+        {
+            Data = await sql.GetDataConfigAsync();
+        }
 
     }
 }
---
+++ MVVM/ModelViews/CreditoVM.cs
@@ -0,0 +1,23 @@
+﻿using AppFinanc.MVVM.Model.SQLite;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFinanc.MVVM.ModelViews
+{
+    public partial class CreditoVM : ObservableObject
+    {
+        [ObservableProperty]
+        private bool _isRedButtonEnabled;
+
+        private SqliteService sql;
+
+        public CreditoVM()
+        {
+            sql = SqliteService.GetInstance();
+        }
+    }
+}
